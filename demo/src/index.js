aafa902ae86fe87e2e9cd8433f4b7b6a94343f11
Dave Timmins <davetimmins@outlook.com>
2017-11-13 09:26:46 +1300

    add ribbon

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,11 +7,8 @@ function DemoComponent({options}) {
   return (
     <div className="App">      
       <div className="App-header">
-        <h2>Welcome to React</h2>
+        <h2>Welcome to Esri-Loader-React</h2>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
       <EsriLoaderReact 
         options={options} 
         modulesToLoad={['esri/Map', 'esri/views/MapView', 'esri/widgets/ScaleBar']}    
