8b8da080f69e6b64dafcb656cf51c9dea7279ccb
Dave Timmins <davetimmins@outlook.com>
2017-11-14 10:55:03 +1300

    update tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,55 +1,66 @@
-import expect from 'expect'
 import React from 'react'
 import {render, unmountComponentAtNode} from 'react-dom'
+import TestRenderer from 'react-test-renderer';
+import expect, { spyOn } from 'expect';
 
-import EsriLoaderReact from 'src/'
-
-class Demo extends React.Component {
-
-  render() {
-
-    const options = {
-      url: 'https://js.arcgis.com/4.5/'
-    };
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <EsriLoaderReact 
-          options={options} 
-          modulesToLoad={['esri/Map', 'esri/views/MapView']}    
-          onReady={({loadedModules: [Map, MapView], containerNode}) => {
-            new MapView({
-              container: containerNode,
-              map: new Map({basemap: 'oceans'})
-            })
-          }}
-          onError={error => console.error(error)}
-        />
-      </div>
-    );
-  }
-}
+import EsriLoaderReact from 'src/';
 
 describe('EsriLoaderReact', () => {
-  let node
+  let testRenderer;
+  let testInstance;
 
+  let context = {
+    onReadyCallback: function ({loadedModules, containerNode}) {},
+    onErrorCallback: function (error, info) {},
+  };
+
+  const options = {
+    url: 'https://js.arcgis.com/4.5/'
+  };
+  
   beforeEach(() => {
-    node = document.createElement('div')
-  })
+    
+    spyOn(context, 'onReadyCallback');
+    spyOn(context, 'onErrorCallback');
+
+    testRenderer = TestRenderer.create(
+      <EsriLoaderReact 
+        options={options} 
+        modulesToLoad={['esri/Map', 'esri/views/MapView']}    
+        onReady={context.onReadyCallback}
+      >
+        <h2>Welcome to Esri-Loader-React</h2>
+        <h3>blah blah</h3>
+      </EsriLoaderReact>
+    );
+    testInstance = testRenderer.root;
+  });
 
   afterEach(() => {
-    unmountComponentAtNode(node)
-  })
+    testRenderer.unmount();
+  });
 
-  it('shows demo', () => {
-    render(<Demo/>, node, () => {
-      expect(node.textContent).toContain('Welcome to React')
-    })
-  })
+  it('should render children', () => {    
+    expect(testInstance.props.children).toContain(<h2>Welcome to Esri-Loader-React</h2>);  
+    expect(testInstance.props.children).toContain(<h3>blah blah</h3>);  
+  });
+
+  // it('should have called onReady', function () {
+  //   expect(context.onReadyCallback).toHaveBeenCalled();
+  // });
+
+  it('should not have called onError', function () {
+    expect(context.onErrorCallback).toNotHaveBeenCalled();
+  });
+
+  it('should have default props', function () {
+    expect(testInstance.props.renderMapContainer).toEqual(true);
+    expect(testInstance.props.mapContainerClassName).toEqual('map-view');
+    expect(testInstance.props.onError).toExist();    
+  });
+
+  it('should match passed in props', function () {
+    expect(testInstance.props.modulesToLoad).toEqual(['esri/Map', 'esri/views/MapView']);
+    expect(testInstance.props.onReady).toEqual(context.onReadyCallback);
+  });
 })
